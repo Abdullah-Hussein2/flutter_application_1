d6ae7bc992b7175db6d35e2aec88940d49efd38b
Abdullah-Hussein2 <abdallhpp5@gmail.com>
2025-10-07 22:05:26 +0300

    Refactor navigation structure and enhance UI interactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(title: 'Navigation Basics', home: MyApp()));
 }
 
 void printThis() {
   print("TEST");
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const MyApp();
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -45,19 +66,49 @@ class MainApp extends StatelessWidget {
             ),
           ],
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Tooltip(
-              message: "Deposit",
-              child: IconButton(onPressed: printThis, icon: Icon(Icons.wallet)),
+        body: Center(
+          child: ElevatedButton(
+            child: Icon(Icons.wallet, size: 50, color: Colors.black),
+
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => const FirstRoute(),
+                ),
+              );
+            },
+          ),
+        ),
+
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          indicatorColor: Color.fromARGB(255, 12, 53, 125),
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Badge(child: Icon(Icons.notifications_sharp)),
+              label: 'Notifications',
+            ),
+            NavigationDestination(
+              icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+              label: 'Messages',
             ),
           ],
         ),
 
         backgroundColor: Color.fromARGB(255, 12, 53, 125),
       ),
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.black54,
@@ -65,6 +116,21 @@ class MainApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+        backgroundColor: Colors.black12,
+      ),
+      backgroundColor: Color.fromARGB(255, 12, 53, 125),
+      body: Center(child: const Text('Open route')),
     );
   }
 }
